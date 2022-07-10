8126eef5092750c75947db7e4e8d6dde67637938
unknown <neeladri22@gmail.com>
2022-07-10 11:47:03 +0530

    Welcome to User Registration -Regex -MS testing

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problem - Regex Unit Testing");
+            Console.WriteLine("\n");
+        }
+    }
+}
