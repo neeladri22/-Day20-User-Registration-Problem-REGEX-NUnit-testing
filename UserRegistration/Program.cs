71201d1b0de4f43b80e27a1a68ab6ed4ad465060
unknown <neeladri22@gmail.com>
2022-07-10 12:32:31 +0530

    Validation for Last Name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -11,8 +11,11 @@ namespace UserRegistration
             Console.WriteLine("\n");
 
             Program P = new Program();
-
+            // validate first name
             P.Validate_FirstName("Neeladri");
+
+            //Validate last name
+            P.Validate_FirstName("Pulakala");
         }
 
         public bool Validate_FirstName(string firstName)
@@ -21,6 +24,14 @@ namespace UserRegistration
             string FirstNamePattern = @"^[A-Z]{1}[a-zA-Z]{2,}$";
 
             return Regex.IsMatch(firstName, FirstNamePattern);
+        }
+
+        public bool Validate_LastName(string lastName)
+        {
+            //Last name starts with capital and should have min 3 characters
+            string LastNamePattern = @"^[A-Z]{1}[a-zA-Z]{2,}$";
+
+            return Regex.IsMatch(lastName, LastNamePattern);
         }
 
     }
